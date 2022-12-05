ec6b723fc5d1154e11f6eadd19854fef5dac6c50
Leticia Moreno <leticia-moreno@outlook.com.br>
2022-12-05 03:53:38 -0300

    added more genres and constants

---
+++ app/src/main/java/com/leticia/moviesmanager/models/Constants.kt
@@ -0,0 +1,8 @@
+package com.leticia.moviesmanager.models
+
+object Constants {
+    const val EXTRA_MOVIE = "EXTRA_MOVIE"
+    const val VIEW_MOVIE = "VIEW_MOVIE"
+    const val EXTRA_LIST_MOVIE_NAMES = "EXTRA_LIST_MOVIE_NAMES"
+    const val INVALID_MOVIE_ID = -1
+}
---
+++ app/src/main/java/com/leticia/moviesmanager/utils/Genre.java
@@ -0,0 +1,13 @@
+package com.leticia.moviesmanager.utils;
+
+public enum Genre {
+    Horror,
+    Comedy,
+    Action,
+    Drama,
+    Romance,
+    Animated,
+    Documentary,
+    Thriller,
+    Fiction
+}
